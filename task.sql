d8b116c431d9b3201622dbb9dddaf7d30c25db11
KostiukOleksandr <Sanjan101088@gmail.com>
2026-05-11 19:54:37 +0300

    3

--- task.sql
+++ task.sql
@@ -1,12 +1,14 @@
-
 USE ShopDB;
+
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
+
+SET @new_order_id = LAST_INSERT_ID();
 
 START TRANSACTION;
 
-
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (LAST_INSERT_ID(), 1, 1);
-
+VALUES (@new_order_id, 1, 1);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
@@ -14,6 +16,6 @@ WHERE ID = 1;
 
 COMMIT;
 
-
-INSERT INTO Orders (CustomerID, Date)
-VALUES (1, '2023-01-01');
+SELECT * FROM Orders;
+SELECT * FROM OrderItems;
+SELECT * FROM Products;
